bef9d8668914b6036abac1a7c01948560acc4875
Josia Rodriguez <josiarodriguez@gmail.com>
2019-10-23 16:09:28 -0400

    Updates

---
+++ precache-manifest.424a75466c4d208a80d8b2afdc4ffdc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ddf26ed0df226f7b6583c2d3469e889",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "16443df437f9996aeeec",
+    "url": "/robofriends/static/css/main.8f46a87b.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "16443df437f9996aeeec",
+    "url": "/robofriends/static/js/main.53ac31e7.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
